242da43e81061674fa0d1c91dd12273d9f5fd574
Gabriel Reis <gabrielgrds@outlook.com>
2022-11-30 00:15:55 -0400

    day1

---
+++ Day1/script.js
@@ -0,0 +1,36 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if ( numeroUm === stringUm) {
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor e tipo')
+} 
+else if ( numeroUm == stringUm) {
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+}
+else {
+    console.log('As variáveis numeroUm e stringUm são diferentes')
+}
+
+if ( numeroTrinta === stringTrinta) {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e tipo')
+} 
+else if ( numeroTrinta == stringTrinta) {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor, mas tipos diferentes')
+}
+else {
+    console.log('As variáveis numeroTrinta e stringTrinta são diferentes')
+}
+
+if ( numeroDez === stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor e tipo')
+} 
+else if ( numeroDez == stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+}
+else {
+    console.log('As variáveis numeroDez e stringDez são diferentes')
+}
---
+++ readme.md
@@ -0,0 +1,37 @@
+# 7 Days Of Code
+
+Arquivos para challenge do 7 days of code da Rafaella Ballerini.
+
+## Dia 1/7 :sunglasses:
+
+Desafio: Reescreva o código abaixo de maneira que ele imprima as informações de maneira correta, que faça sentido e sem erros.
+
+```javascript
+
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (COMPARAR O numeroUm e a stringUm) {
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes.')
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor.')
+}
+
+if (COMPARAR O numeroTrinta e a stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo.')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo.')
+}
+
+if (COMPARAR O numeroDez e a stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes.')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor.')
+}
+```
+
+## Dia 2/7 
