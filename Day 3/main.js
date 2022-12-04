766270bd9bfc0276c358fb6b7fece6de78e5ca5d
Gabriel Reis <gabrielgrds@outlook.com>
2022-12-04 19:04:46 -0400

    finalizando de forma simples o dia 3

---
+++ Day 3/main.js
@@ -0,0 +1,38 @@
+let linguagem = prompt ("Você gostaria de seguir para backend(1) ou frotnend(2)?");
+
+
+if (linguagem == 1) {
+    let back = prompt ("Sendo assim, você gostaria de apreder C#(1) ou Java(2) ?");
+if (back == 1) {
+    alert ("Legal, C# é realmente divertido!");  
+}
+else if (back == 2){
+    alert ("Legal, Java é realmente divertido!");
+}
+else {
+    alert ("Tenta respoder com o número 1 ou 2");
+}
+}
+else if (linguagem == 2) {
+    var front = prompt ("Sendo assim, você gostaria de aprender React (1) ou Vue(2) ?")
+}
+
+if (front == 1) {
+    alert ("Legal, React é mesmo muito legal!")
+}
+
+else if (front == 2) {
+    alert ("Legal, Vue é mesmo muito divertido!");
+}
+else {
+    alert ("Tente responder com o número 1 ou 2");
+}
+
+
+
+
+
+
+
+//colocar isso em uma página html e css
+//aprender a retornar a pergunta quando responderem errado
