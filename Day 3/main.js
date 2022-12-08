79955afc1d1342fcd04d5630386d92d8f8c8ef25
Gabriel Reis <gabrielgrds@outlook.com>
2022-12-08 17:53:58 -0400

    dia 03 bem mais evoluido

--- Day 3/main.js
+++ Day 3/main.js
@@ -1,32 +1,46 @@
 let linguagem = prompt ("Você gostaria de seguir para backend(1) ou frotnend(2)?");
 
-
-if (linguagem == 1) {
+while (linguagem == "1") {
     let back = prompt ("Sendo assim, você gostaria de apreder C#(1) ou Java(2) ?");
-if (back == 1) {
-    alert ("Legal, C# é realmente divertido!");  
-}
-else if (back == 2){
-    alert ("Legal, Java é realmente divertido!");
-}
-else {
-    alert ("Tenta respoder com o número 1 ou 2");
-}
-}
-else if (linguagem == 2) {
-    var front = prompt ("Sendo assim, você gostaria de aprender React (1) ou Vue(2) ?")
-}
-
-if (front == 1) {
-    alert ("Legal, React é mesmo muito legal!")
+    if (back == 1) {
+        alert ("Legal, C# é realmente divertido!");  
+        linguagem = 3;
+    }
+    else if (back == 2){
+        alert ("Legal, Java é realmente divertido!");
+        linguagem = 3;
+    }
+    else {
+        alert ("Tenta respoder com o número 1 ou 2");
+    }
 }
 
-else if (front == 2) {
-    alert ("Legal, Vue é mesmo muito divertido!");
+while (linguagem == "2") {
+    let front = prompt ("Sendo assim, você gostaria de aprender React (1) ou Vue(2) ?");
+    if (front == 1) {
+        alert ("Legal, React é mesmo muito legal!")
+        linguagem == 4;
+    }
+    else if (front == 2) {
+        alert ("Legal, Vue é mesmo muito divertido!");
+        linguagem == 4;
+    }
+    else {
+        alert ("Tente responder com o número 1 ou 2");
+    }
 }
-else {
+
+if (linguagem !== "3" || linguagem !== "4") {
     alert ("Tente responder com o número 1 ou 2");
+    console.log (linguagem);
 }
+
+else {
+    alert ("teste");
+}
+
+
+//let tecnologia = prompt ("Tem alguma tecnologia que você gostaria de se especializar? 1 para sim e 2 para não.");
 
 
 
